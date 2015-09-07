7527e18d034ebff5b0b524b5fd909efaeea3eccc
Joan Biscarri <joan@biscarri.com>
2015-09-07 23:09:25 +0200

    Rename files and model created

--- app/src/main/java/com/biscarri/guedr/activity/ForecastActivity.java
+++ app/src/main/java/com/biscarri/guedr/activity/ForecastActivity.java
@@ -1,20 +1,21 @@
-package com.biscarri.guedr;
+package com.biscarri.guedr.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.biscarri.guedr.R;
+
+public class ForecastActivity extends AppCompatActivity {
 
     private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forecast);
 
         mImageView = (ImageView) findViewById(R.id.weather_image);
         final ToggleButton button = (ToggleButton) findViewById(R.id.change_system_button);
