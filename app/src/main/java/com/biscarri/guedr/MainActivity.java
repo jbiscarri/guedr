a815e581dd4ab52fdd89c33e52c75626452641b8
Joan Biscarri <joan@biscarri.com>
2015-09-07 22:06:19 +0200

    Changing image with a button

--- app/src/main/java/com/biscarri/guedr/MainActivity.java
+++ app/src/main/java/com/biscarri/guedr/MainActivity.java
@@ -2,6 +2,9 @@ package com.biscarri.guedr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.weather_image);
+        Button changeButton = (Button) findViewById(R.id.change_system_button);
+
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.offline_weather2);
+            }
+        });
+
     }
 
 }
